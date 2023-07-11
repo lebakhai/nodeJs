3aa81a8ec9a27e30426423e14ff1d15ec94bd00e
Le Ba Khai <lebakhai1312@gmail.com>
2023-07-11 10:53:57 +0700

    Setup mvc project

---
+++ mvc/src/index.js
@@ -0,0 +1,25 @@
+// import library
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const handlebar = require('express-handlebars');
+const app = express();
+const sass = require('node-sass');
+
+// variable declaration
+const port = 2008;
+
+// config library
+app.engine('hbs', handlebar.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+// render
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+// open webserver
+app.listen(port, () => {console.log(`Open app success at: http://127.0.0.1:${port}`)})
